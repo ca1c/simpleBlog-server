279d059d0085a322e248b3026cf05ffe756b03ba
ca1c <gegs921@gmail.com>
2022-03-30 11:42:30 -0700

    fixed problem where password was required and not being used

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -7,7 +7,6 @@ const UserSchema = new Schema({
     userId: ObjectId,
     email: { type: String, required: true, unique: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: true},
     dateCreated: Date,
 });
 
